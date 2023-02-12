ffc16d84b628ff03b3f9e822112b95a0defba038
中井孝嘉 <nakaitakashiyoshimi@nakaitakashiyoshiminoMacBook-Pro.local>
2023-02-12 14:40:48 +0900

    20230212作業　カート編集機能

--- src/component/cart/reEditComponent.tsx
+++ src/component/cart/reEditComponent.tsx
@@ -14,24 +14,44 @@ import { cartDataArray, cartData } from '../../types';
 //
 //
 
+// ToDo ケーキ単体Xピース数,ホールの値段の値をカート側に追加しておく
+//
+//
+
 interface Props {
   propsCartData: cartData; //カートデータ
 }
 
 const ReEditComponent = ({ propsCartData }: Props) => {
-  const prise = propsCartData.price;
-  const peaceCount = propsCartData.peaceCount;
+  const price = propsCartData.price || 0;
+  const peaceCount = propsCartData.peaceCount || 0;
   const cartId = propsCartData.cartId;
-  const options = propsCartData.options;
+  const options = propsCartData.options || [];
 
   const [newPeaceCount, setNewPeaceCount] = useState(peaceCount);
   //   const [optionArray, updateOption] = useState<number[]>(options); //トッピングの個数管理
   //   ２
   // ３
   // 4
+  const [witdhOptionPrice, updateOptioPrice] = useState<number>(price);
+  const [countOfPeace, updateCountOfPeace] = useState<number>(peaceCount); //ピース数
+  // const [cakePrice, updateCakePrice] = useState<number>(peacePrice); //ケーキの値段(ピース合計)
 
   // ・・・・・・・・・・・・・・・・・・・・
+  // 項目名
+  const ItemText = styled.p``;
+  const ItemTextWrapper = styled.div``;
+
+  const SwitchingButton = styled.button``;
+
+  const SwitchButtonWrapper = styled.div`
+    display: flex;
+  `;
+  const PeaceWrapper = styled.div`
+    display: flex;
+  `;
   //   調整ボタンとそのテキスト類のエリア
+
   const OptionWrapper = styled.div`
     display: flex;
   `;
@@ -41,42 +61,66 @@ const ReEditComponent = ({ propsCartData }: Props) => {
   const AddPriceText = styled.p``;
   // ・・・・・・・・・・・・・・・・・・・・
 
+  // ..................
+  // トッピングの更新用の関数
+  const upDateArray = (index: number, val: number) => {
+    // ０よりは小さくしない
+    if (val < 0 && options[index].count == 0) {
+      return;
+    }
+
+    //インデックスを指定して更新配列を更新
+    const newArray = options.map((it, it_index) => {
+      return it_index == index
+        ? { name: it.name, param: it.param, count: it.count + val }
+        : it;
+    });
+
+    // stateで更新実施
+    // updateOption(newArray);  書き換え！！！
+  };
+  // ..............]
+
   //   ・・・・・・・・・・・・・・・・・・＝＞編集したデータを更新するDispatchを用意して実行する
   //カートへの追加実行の関数
-  //   const addCartFunction = () => {
-  //     // optionArrayと引数のトッピング情報などからデータを作ってプッシュする
+  const addCartFunction = () => {
+    // optionArrayと引数のトッピング情報などからデータを作ってプッシュする
 
-  //     const optionsData = options?.map((it, index) => {
-  //       const result = {
-  //         name: it.name,
-  //         param: it.param,
-  //         count: optionArray[index],
-  //       };
+    // カートのトッピング配列を使用するので不要な認識↓
+    // const optionsData = options?.map((it, index) => {
+    //   const result = {
+    //     name: it.name,
+    //     param: it.param,
+    //     count: optionArray[index],
+    //   };
 
-  //       return result;
-  //     });
+    //   return result;
+    // });
 
-  //     const pushData = {
-  //       data: {
-  //         price: witdhOptionPrice,
-  //         itemName: itemInfoName,
-  //         imageUrl: propImageUrl1,
-  //         imageUr2: propImageUrl2,
-  //         // price?: number;
-  //         peaceCount: countOfPeace,
+    const pushData = {
+      data: {
+        // price: witdhOptionPrice,
+        // itemName: itemInfoName,
+        // imageUrl: propImageUrl1,
+        // imageUr2: propImageUrl2,
+        // price?: number;
+        // peaceCount: countOfPeace,
 
-  //         // code?: string;
-  //         // discription?: string;
-  //         options: optionsData,
-  //       },
-  //     };
+        // code?: string;
+        // discription?: string;
+        options: options,
+      },
+    };
 
-  //     console.log('dispatch addCart!!');
-  //     dispatch(addCart(pushData)); //dispatch カートへ追加
-  //     visibleAddedFunction(); //カート追加画面表示
+    // ここでDispatch実行と編集モーダルとじる機能実装
 
-  //     // chyangeAddFlg(true);
-  //   };
+    // console.log('dispatch addCart!!');
+
+    // dispatch(addCart(pushData)); //dispatch カートへ追加
+    // visibleAddedFunction(); //カート追加画面表示
+
+    // chyangeAddFlg(true);
+  };
   // ・・・・・・・・・・・・・・・・・・・・・・・
 
   //   ・・・・・・・・・・・・・・・・・・・・・・
@@ -131,37 +175,37 @@ const ReEditComponent = ({ propsCartData }: Props) => {
 
   //   ////////////////
   //    // オプション用のボタンコンポーネント
-  //  const optionComponent = (index: number, addPrice: number) => {
-  //   const n = optionArray[index]; //現在のトッピング数
-  //   const component = (
-  //     <>
-  //       <OptionWrapper>
-  //         <DownButton onClick={() => upDateArray(index, -1)}>-</DownButton>
-  //         <CountNumber>{n}</CountNumber>
-  //         <UpButton onClick={() => upDateArray(index, 1)}>+</UpButton>
-  //         {/* オプション数※パラメータで金額表示 */}
-  //         <AddPriceText> +{addPrice * n}</AddPriceText>
-  //       </OptionWrapper>
-  //     </>
-  //   );
+  const optionComponent = (index: number, addPrice: number) => {
+    const n = options[index].count; //現在のトッピング数
+    const component = (
+      <>
+        <OptionWrapper>
+          <DownButton onClick={() => upDateArray(index, -1)}>-</DownButton>
+          <CountNumber>{n}</CountNumber>
+          <UpButton onClick={() => upDateArray(index, 1)}>+</UpButton>
+          {/* オプション数※パラメータで金額表示 */}
+          <AddPriceText> +{addPrice * n}</AddPriceText>
+        </OptionWrapper>
+      </>
+    );
 
-  //   return component;
-  // };
+    return component;
+  };
   // ///////////////////
 
   //   ・・・・・・・・・・・・・・・・・・・・・・・・・・
   //    // オプションボタンコンポーネントをオプションの配列分作成する
-  //  const optionCompoList = options?.map((it, index) => {
-  //   // index を取得して更新関数まで渡す
-  //   const result = (
-  //     <>
-  //       <ItemText>{it.name}</ItemText>
-  //       {optionComponent(index, it.param)}
-  //     </>
-  //   );
+  const optionCompoList = options?.map((it, index) => {
+    // index を取得して更新関数まで渡す
+    const result = (
+      <>
+        <ItemText>{it.name}</ItemText>
+        {optionComponent(index, it.param)}
+      </>
+    );
 
-  //   return result;
-  // });
+    return result;
+  });
   // ・・・・・・・・・・・・・・・・・・・・・・・・・・・・・・・
 
   // 編集用のボタンコンポ
